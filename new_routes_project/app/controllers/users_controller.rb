ca3f1dadffac6602acdb3c3d2604ba77e7084f26
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-04-12 14:03:10 -0400

    finished phase 4

--- new_routes_project/app/controllers/users_controller.rb
+++ new_routes_project/app/controllers/users_controller.rb
@@ -1,14 +1,36 @@
 class UsersController < ApplicationController
     def index
-        render json: params
+        @users = User
+        render json: @users
     end
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+          render json: user
+        else
+          render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
         render json: params
-    end  
+    end 
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(users_params)
+            redirect_to users_params
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+    
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_url
+    end
 
 end
