a77405182747e6a2fbfa1bf6a67e5a1d48d4eb0d
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-04-13 16:55:43 -0400

    fixed some syntax

--- new_routes_project/config/routes.rb
+++ new_routes_project/config/routes.rb
@@ -5,19 +5,24 @@ Rails.application.routes.draw do
   # root "articles#index"
   # resources :users
   # get all users
-  get '/users', to: 'users#index', as: 'users'
-  post '/users', to: 'users#create'
-  get '/users/:id', to: 'users#show', as: 'user'
-  patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  # get '/users', to: 'users#index', as: 'users'
+  # post '/users', to: 'users#create'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
+  resources :users, only: [:index, :create, :show, :update, :destroy]
 
-  get '/artworks/:id', to: 'artworks#show'
-  post '/artworks', to: 'artworks#create'
-  patch '/artworks/:id', to: 'artworks#update'
-  delete '/artworks/:id', to: 'artworks#destroy'
+  # get '/artworks/:id', to: 'artworks#show'
+  # post '/artworks', to: 'artworks#create'
+  # patch '/artworks/:id', to: 'artworks#update'
+  # delete '/artworks/:id', to: 'artworks#destroy'
+  # get '/users/:user_id/artworks', to: 'artworks#index'
+  resources :artworks, only: [:show, :create, :update, :destroy]
+  
+  resources :artwork_shares, only: [:create, :destroy]
 
   resources :users do
     resources:artworks, only [:index]
   end
-  
+
 end
